4971906c7049583e3941cfa8380b9eeab98278a4
Rémi Defoor <remi.defoor@student.howest.be>
2022-11-29 13:10:18 +0100

    feat(db): create door seeder

---
+++ database/door-seeder.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function createDoor(): Promise<void> {
+    await prisma.door.create({
+        data: {
+            isLocked: true,
+            unlockCode: '1',
+            lockAt: '22:00:00',
+            unlockAt: '06:00:00',
+        },
+    });
+}
+
+export async function seed(): Promise<void> {
+    await createDoor();
+    const doors = await prisma.door.findMany();
+    console.info('New state:\n', doors);
+}
+
+export default {
+    seed,
+};
