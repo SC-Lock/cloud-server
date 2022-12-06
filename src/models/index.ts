b800658046879a51be2b270aeee09f7af9aebe1b
Rémi Defoor <remi.defoor@student.howest.be>
2022-12-06 11:44:37 +0100

    feat(door log): create entry on door update

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,5 @@
 import { Door } from './door.interface';
+import { DoorLog } from './door-log.interface';
+import { DoorLogCreate } from './door-log-create.interface';
 
-export { Door };
+export { Door, DoorLog, DoorLogCreate };
