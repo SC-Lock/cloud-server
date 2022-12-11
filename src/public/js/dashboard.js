2268d3e53b8c63a226621410510b791c88bcff39
Rémi Defoor <remi.defoor@student.howest.be>
2022-12-11 16:25:57 +0100

    chore: remove test chart

--- src/public/js/dashboard.js
+++ src/public/js/dashboard.js
@@ -6,16 +6,9 @@ document.addEventListener('DOMContentLoaded', init);
 
 async function init() {
     const doorLogs = await getDoorLogs();
-    console.log(doorLogs);
-    drawTestChart();
 }
 
 async function getDoorLogs() {
     const res = await fetch('/api/doors/1/logs');
     return await res.json();
-}
-
-function drawTestChart() {
-    const ctx = document.querySelector('#test-chart');
-    new Chart(ctx, ChartConfigurations.testChart);
 }
