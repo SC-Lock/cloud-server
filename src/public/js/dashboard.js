32cc820fb8ae184daa6667e19539117f87ba32d1
Rémi Defoor <remi.defoor@student.howest.be>
2022-12-11 14:13:15 +0100

    feat(door monitor dashboard): get door logs

--- src/public/js/dashboard.js
+++ src/public/js/dashboard.js
@@ -1,32 +1,21 @@
 'use strict';
 
+import ChartConfigurations from './chartConfigurations.js';
+
 document.addEventListener('DOMContentLoaded', init);
 
-function init() {
+async function init() {
+    const doorLogs = await getDoorLogs();
+    console.log(doorLogs);
     drawTestChart();
+}
+
+async function getDoorLogs() {
+    const res = await fetch('/api/doors/1/logs');
+    return await res.json();
 }
 
 function drawTestChart() {
     const ctx = document.querySelector('#test-chart');
-
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [
-                {
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    borderWidth: 1,
-                },
-            ],
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true,
-                },
-            },
-        },
-    });
+    new Chart(ctx, ChartConfigurations.testChart);
 }
