c0134ba27d85855633434c28177388caf8c407c3
Rémi Defoor <remi.defoor@student.howest.be>
2022-12-11 09:39:17 +0100

    feat: setup public resources

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const PORT = process.env.EXPRESS_PORT || 3000;
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
