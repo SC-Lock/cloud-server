6c0890ab72fb7d6ea5293d31f84b7dc494be9e9b
Rémi Defoor <remi.defoor@student.howest.be>
2022-12-12 18:25:05 +0100

    feat(door monitor dashboard): create time-locked chart

--- src/public/js/chartConfigurations.js
+++ src/public/js/chartConfigurations.js
@@ -1,1 +1,38 @@
-export default {};
+export function getTimeLockedChart(doorLogs) {
+    const lastDoorLogs = doorLogs.slice(-20);
+    const timeValues = lastDoorLogs.map((doorLog) => doorLog.createdAt);
+    const lockedValues = lastDoorLogs.map((doorLog) => doorLog.isLocked);
+    return {
+        type: 'line',
+        data: {
+            labels: timeValues,
+            datasets: [
+                {
+                    label: 'locked',
+                    data: lockedValues,
+                    backgroundColor: '#248886',
+                    borderColor: '#248886',
+                },
+            ],
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    text: 'Most recent updates',
+                },
+            },
+            scales: {
+                y: {
+                    grid: {
+                        display: false,
+                    },
+                },
+            },
+        },
+    };
+}
+
+export default {
+    getTimeLockedChart,
+};
